6e9bf5f192d1b745aabd687bd0b6fe96b787b516
Nicolai Parlog <nipa@codefx.org>
2018-12-04 07:59:21 +0100

    Remove fixed TODO
    With option -jar, class path is ignored

--- 00-class-path/run.sh
+++ 00-class-path/run.sh
@@ -5,7 +5,3 @@ source ../executables.sh
 echo "--- LAUNCHING ---"
 
 $JAVA --class-path 'libs/*':'jars/*' monitor.Main
-
-# TODO: figure out why running as jar leads to not seeing libs on class-path
-#       (I guess manifest)
-#$JAVA --class-path 'libs/*' -jar jars/monitor.jar
